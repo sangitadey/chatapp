d94348a232ff2a5889ce4ac6fe2c7e38d0859a94
sangita dey <deymunai2000@gmail.com>
2022-06-24 19:17:31 +0530

    setting up paths

---
+++ model/people.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const peopleSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true, //to record the chages in our database we use this here.
+  }
+);
+
+const People = mongoose.model("People", peopleSchema);
+
+module.exports = People;
--- router/usersRouter.js
+++ router/usersRouter.js
@@ -3,6 +3,13 @@ const { Router } = require("express");
 const express = require("express");
 //internal imports
 const { getusers } = require("../controller/usersController");
+const avatarupload = require("../middlewares/users/avatarupload");
 const router = express.Router();
 router.get("/", getusers);
+//add users
+const {
+  addUserValidators,
+  addUserValidationHandler,
+} = require("../middlewares/users/usersvalidator");
+router.post("/", avatarupload, addUserValidators, addUserValidationHandler);
 module.exports = router;
